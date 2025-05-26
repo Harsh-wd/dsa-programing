0c0bd4dc5abce42cb4b0d69174d5709cbad5b9c4
Harsh-wd <harshbansal.it26@gmail.com>
2025-05-26 21:25:56 +0530

    pattern4 updated

--- BASIC/pattern4.cpp
+++ BASIC/pattern4.cpp
@@ -6,13 +6,12 @@ int main()
     int n=3;
     for(int i=0;i<n;i++)
     {
-        for(int j=1;j<=i+1;j++)
+        for(int j=0;j<i+1;j++)
         {
-            cout<<j<<"";
+            cout<<j+1;
         }
         cout<<endl;
     }
-    return 0;
 }
 
 //1
